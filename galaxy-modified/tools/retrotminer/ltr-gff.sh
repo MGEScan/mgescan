20fd78b1d525ed264d979ca056439d2c29f67e10
Hyungro Lee <lee212@indiana.edu>
2014-06-15 02:15:12 -0400

    ltr is separated into 4 of 4 steps.

---
+++ galaxy-modified/tools/retrotminer/ltr-gff.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+user_dir=/u/lee212
+#source ~/virtualenv/retrotminer/bin/activate
+script_program=`which python`
+script=$user_dir/github/retrotminer/retrotminer/ltr/toGFF.py
+
+input_file=$1
+output_file=$2
+
+#move to the working directory
+work_dir=`dirname $script`
+cd $work_dir
+
+#create directory for input and output
+t_dir=`mktemp -p input -d` #relative path
+input_dir="$work_dir/$t_dir/seq" # full path
+output_dir="$work_dir/$t_dir/data"
+mkdir -p $input_dir
+mkdir -p $output_dir
+
+#make a copy of input
+/bin/cp $input_file $input_dir/$input_file
+
+#run
+$script_program $script $input_dir/$input_file $output_file
+
+if [ $? -eq 0 ]
+then
+	rm -rf $work_dir/$t_dir
+else
+	cp -pr $work_dir/$t_dir $work_dir/error-cases/
+fi
