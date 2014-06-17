168afd8938de111685ad2200affbd3dae9d9504d
Hyungro Lee <lee212@indiana.edu>
2014-06-17 00:22:18 -0400

    nonltr has been divided.

--- retrotminer/rtm-nonltr.py
+++ retrotminer/rtm-nonltr.py
@@ -24,7 +24,8 @@ class nonLTR(RetroTMiner):
     main_dir = "nonltr"
     cmd_hmm = main_dir + "/run_hmm.pl"
     cmd_post_process = main_dir + "/post_process.pl"
-    cmd_validate_q_value = main_dir + "/post_proces2.pl"
+    cmd_validate_q_value = main_dir + "/post_process2.pl"
+    cmd_togff = main_dir + "/toGFF.py"
 
     processes = set()
     max_processes = 5
@@ -44,15 +45,19 @@ class nonLTR(RetroTMiner):
     def run(self):
         p1 = Process(target=self.forward_strand)
         p1.start()
+
+        # Reverse complement before backward strand
         self.reverse_complement()
         p2 = Process(target=self.backward_strand)
         p2.start()
+
         p1.join()
         p2.join()
 
         # validation for q value
-        self.post_processing()
+        self.post_processing2()
 
+        # convert to gff3
         self.toGFF()
 
     def forward_strand(self):
@@ -77,6 +82,8 @@ class nonLTR(RetroTMiner):
             if p.poll() is None:
                 p.wait()
 
+        self.post_processing_after_forward_strand()
+
     def backward_strand(self):
         print "Running backward...\n"
         mypath = self.minus_dir
@@ -99,6 +106,23 @@ class nonLTR(RetroTMiner):
             if p.poll() is None:
                 p.wait()
 
+        self.post_processing_after_reverse_strand()
+
+    def post_processing_after_forward_strand(self):
+        self.post_processing(self.plus_out_dir, self.plus_dir, 0)
+
+    def post_processing_after_reverse_strand(self):
+        self.post_processing(self.minus_out_dir, self.minus_dir, 1)
+
+    def post_processing(self, out_dir, dir, reverse_yn):
+        os.remove(self.get_abspath(out_dir + "/out1/aaaaa"))
+        os.remove(self.get_abspath(out_dir + "out1/bbbbb"))
+        os.remove(self.get_abspath(out_dir + "out1/ppppp"))
+        os.remove(self.get_abspath(out_dir + "out1/qqqqq"))
+        cmd = self.cmd_post_process + (" --dna=%s --out=%s --rev=%s" %
+                (dir, out_dir, reverse_yn))
+        self.run_cmd(cmd)
+
     def reverse_complement(self):
         mypath = self.genome_dir
         for (dirpath, dirnames, filenames) in os.walk(mypath):
@@ -110,11 +134,19 @@ class nonLTR(RetroTMiner):
             file_path = self.get_abspath(dirpath + "/" + name)
             reverse_complement_fasta(file_path, directory)
 
-    def post_processing(self):
-        return
+    def post_processing2(self):
+
+        cmd = self.cmd_validate_q_value + \
+                " --data_dir=%(data_dir)s --hmmerv=%(hmmerv)s"
+        self.run_cmd(cmd)
 
     def toGFF(self):
-        return
+
+        # gff3
+        self.nonltr_out_path = self.get_abspath(self.data_dir + "/info/full/")
+        self.nonltr_gff_path = self.get_abspath(self.data_dir + "/info/nonltr.gff3")
+        cmd = self.cmd_togff + " %(nonltr_out_path)s %(nonltr_gff_path)s"
+        res = self.run_cmd(cmd)
 
 def main():
     arguments = docopt(__doc__, version="rtm-nonltr 0.1")
