ba0ae081b93cb7c36ff45edf30f6fab83dc69c35
Hyungro Lee <lee212@indiana.edu>
2014-10-09 12:33:42 -0400

    split a large filr added to manage scaffold file

--- mgescan/split.py
+++ mgescan/split.py
@@ -1,8 +1,24 @@
+"""split.py: separate a large file
+
+Usage:
+    split.py <genome_dir> [--output=<data_dir>]
+    split.py (-h | --help)
+    split.py --version
+
+Options:
+    -h --help   Show this screen.
+    --version   Show version.
+    --output=<data_dir> Directory results will be saved
+
+"""
+from docopt import docopt
 from Bio import SeqIO
-record_iter = SeqIO.parse(open("large.fasta"),"fasta")
-for val in record_iter:
-    filename=val.id+".fa"
-    handle = open(filename, "w")
-    SeqIO.write([val], handle, "fasta")
-    handle.close()
+def split_file(filename, output_path):
+
+    record_iter = SeqIO.parse(open("large.fasta"),"fasta")
+    for val in record_iter:
+        filename=val.id+".fa"
+        handle = open(filename, "w")
+        SeqIO.write([val], handle, "fasta")
+        handle.close()
 
