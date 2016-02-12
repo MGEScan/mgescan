d6367bb285bba7a14d9f74ac1598443abcab6780
Hyungro Lee <hroe.lee@gmail.com>
2016-02-12 09:45:29 +0000

    fix indentation error

--- mgescan/nonltr/toGFF.py
+++ mgescan/nonltr/toGFF.py
@@ -6,31 +6,32 @@ import glob
 import re
 
 if len(sys.argv) < 3:
-	print sys.argv[0], "outdir new-GFF-file"
-	sys.exit()
-	
+    print sys.argv[0], "outdir new-GFF-file"
+    sys.exit()
+
 def readFASTA(filename):
- fastalines = []
- f = open(filename)
- lines = f.readlines()
- f.close()
- if lines[0][0] != '>':
-  sys.exit("Invalid FASTA file: "+filename)
- seq = ""
- header = ""
- for line in lines:
-  if len(line) > 0:
-   if line[0]=='>':
+    fastalines = []
+    f = open(filename)
+    lines = f.readlines()
+    f.close()
+    
+    if lines[0][0] != '>':
+        sys.exit("Invalid FASTA file: "+filename)
+    seq = ""
+    header = ""
+    for line in lines:
+        if len(line) > 0:
+            if line[0]=='>':
+                if seq != "":
+                    fastalines.append([header,seq])
+                    seq = ""
+                header = line.strip()[1:]
+            else:
+                line = re.sub(r'\s','',line)
+                seq = seq + line
     if seq != "":
-     fastalines.append([header,seq])
-     seq = ""
-    header = line.strip()[1:]
-   else:
-    line = re.sub(r'\s','',line)
-    seq = seq + line
- if seq != "":
-  fastalines.append([header,seq])
- return fastalines
+        fastalines.append([header,seq])
+    return fastalines
 
 outfile = open(sys.argv[2], "w")
 #print >>outfile, "##gff-version 3"
@@ -42,13 +43,13 @@ print >>outfile, "track name=nonLTR description=\"MGEScan-nonLTR\" color=255,0,0
 # Drosophila_melanogaster.BDGP5.dna.chromosome.2L.fa_51   16141217        16141492 16146090        16146365        +       276     276     5149    6.02e-129 AAAAT   AAAAT   TGT     ACA     TGT     ACA D
 #
 for cladeDir in glob.glob( os.path.join(sys.argv[1], '*') ):
-	basename = os.path.basename(cladeDir)
-	filepath = cladeDir + "/" + basename + ".dna"
-	fastalist = readFASTA(filepath)
-	for fastaitem in fastalist:
-		header = fastaitem[0] # Drosophila_melanogaster.BDGP5.dna.chromosome.2L.fa_51
-		seq = fastaitem[1]
-		# seqid = header[:header.find("_")]
+    basename = os.path.basename(cladeDir)
+    filepath = cladeDir + "/" + basename + ".dna"
+    fastalist = readFASTA(filepath)
+    for fastaitem in fastalist:
+        header = fastaitem[0] # Drosophila_melanogaster.BDGP5.dna.chromosome.2L.fa_51
+        seq = fastaitem[1]
+        # seqid = header[:header.find("_")]
         seqid = header[:header.rfind("_")]
         try:
             # dm3_gold_chr4_1
